e927c5b0df05329a60e4705794629ae93e3c5b54
tinchoo290 <perez.martinmgl@gmail.com>
2021-10-30 23:31:18 +0200

    primer desafio

---
+++ dclase1.js
@@ -0,0 +1,11 @@
+let nombre = prompt ("Ingrese su NOMBRE");
+let apellido = prompt ("Ingrese su APELLIDO");
+let nombreCompleto = nombre + apellido;
+
+console.log("Hola " + nombreCompleto);
+
+let dni = prompt ("Ingresar su DNI");  
+
+console.log("Su DNI es: " + dni);
+
+alert ("Aquí todo comienza");
